eb91471cc4a4afacdc0f3bab3aa408d7f7e0101d
Arkadiusz Kubaczkowski <arek.kubaczkowski@gmail.com>
2026-03-24 09:59:07 +0100

    feat: add Effects SDK integration hooks and video processing improvements
    - Add CapturerProvider + CapturerFactoryInterface for external capturer registration
    - Add setVideoFrameProcessor:forTrackId: direct API on iOS
    - Fix ProcessorProvider thread safety with concurrent dispatch queue
    - Add TextureView renderer support
    - Custom video transforms and cropping fixes
    - Fix background image scaling

--- ios/RCTWebRTC/videoEffects/ProcessorProvider.m
+++ ios/RCTWebRTC/videoEffects/ProcessorProvider.m
@@ -3,21 +3,31 @@
 @implementation ProcessorProvider
 
 static NSMutableDictionary<NSString *, NSObject<VideoFrameProcessorDelegate> *> *processorMap;
+static dispatch_queue_t processorMapQueue;
 
 + (void)initialize {
     processorMap = [[NSMutableDictionary alloc] init];
+    processorMapQueue = dispatch_queue_create("com.webrtc.processorprovider", DISPATCH_QUEUE_CONCURRENT);
 }
 
 + (NSObject<VideoFrameProcessorDelegate> *)getProcessor:(NSString *)name {
-    return [processorMap objectForKey:name];
+    __block NSObject<VideoFrameProcessorDelegate> *result = nil;
+    dispatch_sync(processorMapQueue, ^{
+        result = [processorMap objectForKey:name];
+    });
+    return result;
 }
 
 + (void)addProcessor:(NSObject<VideoFrameProcessorDelegate> *)processor forName:(NSString *)name {
-    [processorMap setObject:processor forKey:name];
+    dispatch_barrier_sync(processorMapQueue, ^{
+        [processorMap setObject:processor forKey:name];
+    });
 }
 
 + (void)removeProcessor:(NSString *)name {
-    [processorMap removeObjectForKey:name];
+    dispatch_barrier_sync(processorMapQueue, ^{
+        [processorMap removeObjectForKey:name];
+    });
 }
 
 @end
